5b21841af0c6f347ab719edd9a86b757e388d694
adangzlz22 <adangzlz22@gmail.com>
2021-06-08 09:28:45 -0700

    otro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,39 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+ 
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./page/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./page/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
+  {
+    path: 'bitacora',
+    loadChildren: () => import('./page/bitacora/bitacora.module').then( m => m.BitacoraPageModule)
+  },
+  {
+    path: 'editar-bitacora',
+    loadChildren: () => import('./page/editar-bitacora/editar-bitacora.module').then( m => m.EditarBitacoraPageModule)
+  },
+  {
+    path: 'certificar-inspeccion',
+    loadChildren: () => import('./page/certificar-inspeccion/certificar-inspeccion.module').then( m => m.CertificarInspeccionPageModule)
+  },
+  {
+    path: 'obtener-vehiculos',
+    loadChildren: () => import('./page/obtener-vehiculos/obtener-vehiculos.module').then( m => m.ObtenerVehiculosPageModule)
+  },
+  {
+    path: 'modal-remolque',
+    loadChildren: () => import('./page/modal-remolque/modal-remolque.module').then( m => m.ModalRemolquePageModule)
   },
 ];
 
